0ed6e6abe4d30c686fcb785718f58c4924057987
clement <clement@antidots.com>
2019-03-26 16:21:40 +0100

    commented out seed action

--- server/src/index.js
+++ server/src/index.js
@@ -3,19 +3,19 @@ const logger = require('./logger')
 const app = require('./app')
 const port = app.get('port')
 const server = app.listen(port)
-const seed = require('./seed')
+// const seed = require('./seed')
 
-;(async () => {
-  console.log(`[seeder] start seeding database`)
-  await seed(app)
-    .then(() => {
-      console.log(`[seeder] database seeded`)
-    })
-    .catch(e => {
-      console.error(`[seeder] unable to seed database`)
-      console.error(e)
-    })
-})()
+// ;(async () => {
+//   console.log(`[seeder] start seeding database`)
+//   await seed(app)
+//     .then(() => {
+//       console.log(`[seeder] database seeded`)
+//     })
+//     .catch(e => {
+//       console.error(`[seeder] unable to seed database`)
+//       console.error(e)
+//     })
+// })()
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
