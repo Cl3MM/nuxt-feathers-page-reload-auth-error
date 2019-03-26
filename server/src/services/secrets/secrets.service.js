cf80102e0090fb14ebcd6aaa5c043790db294f3e
clement <clement@antidots.com>
2019-03-26 16:21:14 +0100

    switch to feathers-memory

--- server/src/services/secrets/secrets.service.js
+++ server/src/services/secrets/secrets.service.js
@@ -1,22 +1,44 @@
 // Initializes the `secrets` service on path `/secrets`
-const createService = require('feathers-nedb');
-const createModel = require('../../models/secrets.model');
-const hooks = require('./secrets.hooks');
+const createService = require('feathers-memory')
+const hooks = require('./secrets.hooks')
+const loremIpsum = require('lorem-ipsum')
 
-module.exports = function (app) {
-  const Model = createModel(app);
-  const paginate = app.get('paginate');
+const rndAr = (n = 100, len = 10) =>
+  Array.from({ length: len }, () => Math.floor(Math.random() * n))
+
+module.exports = function(app) {
+  const paginate = app.get('paginate')
 
   const options = {
-    Model,
-    paginate
-  };
+    paginate,
+    id: '_id',
+    multi: true,
+    store: rndAr(10, 20).reduce(
+      (h, val, idx) => (
+        (h[idx] = {
+          _id: idx,
+          title: loremIpsum({
+            count: 1,
+            sentenceLowerBound: 4,
+            sentenceUpperBound: 10,
+            units: 'sentences'
+          }),
+          message: loremIpsum({
+            units: 'paragraphs'
+          }),
+          userId: idx % 4 === 0 ? 0 : idx % 4 === 1 ? 1 : null
+        }),
+        h
+      ),
+      {}
+    )
+  }
 
   // Initialize our service with any options it requires
-  app.use('/secrets', createService(options));
+  app.use('/secrets', createService(options))
 
   // Get our initialized service so that we can register hooks
-  const service = app.service('secrets');
+  const service = app.service('secrets')
 
-  service.hooks(hooks);
-};
+  service.hooks(hooks)
+}
--- server/src/services/users/users.service.js
+++ server/src/services/users/users.service.js
@@ -1,15 +1,14 @@
 // Initializes the `users` service on path `/users`
-const createService = require('feathers-nedb')
-const createModel = require('../../models/users.model')
+const createService = require('feathers-memory')
 const hooks = require('./users.hooks')
 
 module.exports = function(app) {
-  const Model = createModel(app)
   const paginate = app.get('paginate')
 
   const options = {
-    Model,
-    paginate
+    paginate,
+    id: '_id',
+    multi: true
   }
 
   // Initialize our service with any options it requires
@@ -17,6 +16,13 @@ module.exports = function(app) {
 
   // Get our initialized service so that we can register hooks
   const service = app.service('users')
-
   service.hooks(hooks)
+  ;['admin', 'user'].map(u =>
+    service.create({
+      email: `${u}@example.com`,
+      password: u,
+      name: u,
+      dob: new Date()
+    })
+  )
 }
